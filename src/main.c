46da73bf381934278e627069ca11351a2d45c124
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-02-23 12:09:40 -0300

    changed map teste.cub configs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: etomiyos <etomiyos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:07:58 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/02/23 02:19:20 by etomiyos         ###   ########.fr       */
+/*   Updated: 2023/02/23 11:37:03 by etomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 	invalid_args(&cub3d, argc, argv);
 	init(argv, &cub3d);
 	get_data(&cub3d);
-	loop(&cub3d);
+	// loop(&cub3d);
 	destroy_all(&cub3d);
 	return (EXIT_SUCCESS);
 }
