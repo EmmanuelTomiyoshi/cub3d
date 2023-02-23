753e5559de094a8be74f324a834e84be32746d26
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-02-23 18:30:31 -0300

    added criterion tests

---
+++ tests/test.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etomiyos <etomiyos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 18:10:53 by etomiyos          #+#    #+#             */
+/*   Updated: 2023/02/23 18:22:01 by etomiyos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../inc/cub3d.h"
+# include <criterion/criterion.h>
+# include <criterion/internal/assert.h>
+# include <criterion/internal/test.h>
+
+Test(suite, Test)
+{
+    cr_assert(is_floor_or_ceiling('C'));
+}
