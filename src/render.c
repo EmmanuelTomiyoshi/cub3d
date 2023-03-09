44abe8816962a98a0545653451ae6ad8c94a9fa6
Matomomitsu <matomomitsu@gmail.com>
2023-03-09 18:14:59 -0300

    teste github actions

--- src/render.c
+++ src/render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etomiyos <etomiyos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:42:03 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/03/08 20:41:51 by etomiyos         ###   ########.fr       */
+/*   Updated: 2023/03/09 18:14:22 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,6 @@ int	end_loop(t_cub3d *c)
 void	loop(t_cub3d *c)
 {
 	mlx_hook(c->mlx.win, 17, 0, end_loop, c);
-	mlx_expose_hook(c->mlx.win, &draw, c);
+	mlx_expose_hook(c->mlx.win, &render, c);
 	mlx_loop(c->mlx.ptr);
 }
