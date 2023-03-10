fdecd19f5f5e9765729d2a3c096d8514e7d41a86
Matomomitsu <matomomitsu@gmail.com>
2023-03-10 16:32:59 -0300

    movement

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:42:03 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/03/10 10:07:07 by mtomomit         ###   ########.fr       */
+/*   Updated: 2023/03/10 15:08:12 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,6 @@ void	loop(t_cub3d *c)
 {
 	mlx_hook(c->mlx.win, 17, 0, end_loop, c);
 	mlx_key_hook(c->mlx.win, key_handle, c);
-	mlx_expose_hook(c->mlx.win, &draw, c);
+	mlx_loop_hook(c->mlx.ptr, &draw, c);
 	mlx_loop(c->mlx.ptr);
 }
