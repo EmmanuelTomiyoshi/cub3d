a77e93a3c619bb76756ae4f82c53dcabcf003732
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-03-12 14:43:41 -0300

    changed camera speed to more fluid

--- src/player/camera.c
+++ src/player/camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: etomiyos <etomiyos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:01:38 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/03/10 15:39:00 by mtomomit         ###   ########.fr       */
+/*   Updated: 2023/03/12 14:27:53 by etomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	look_left(t_cub3d *c)
 {
-	rotate_vector(&c->player.dir, 1);
-	rotate_vector(&c->player.camera.plane, 1);
+	rotate_vector(&c->player.dir, 3.14);
+	rotate_vector(&c->player.camera.plane, 3.14);
 }
 
 void	look_right(t_cub3d *c)
 {
-	rotate_vector(&c->player.dir, 359);
-	rotate_vector(&c->player.camera.plane, 359);
+	rotate_vector(&c->player.dir, 356.86);
+	rotate_vector(&c->player.camera.plane, 356.86);
 }
