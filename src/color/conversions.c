ea6966ed707783d35a22376d3c0c951feb236792
Matomomitsu <matomomitsu@gmail.com>
2023-03-14 16:14:37 -0300

    adding functions

--- src/color/conversions.c
+++ src/color/conversions.c
@@ -6,31 +6,17 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 19:47:01 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/03/14 15:51:34 by mtomomit         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:04:53 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	my_pixel_put(t_cub3d *c, int x, int y, int color)
+void	itorgb(int color, t_rgb *result)
 {
-	char	*dst;
-
-	if (x <= c->mlx.win.width && y <= c->mlx.win.height && y >= 0 && x >= 0)
-	{
-		dst = c->mlx.img.addr + (y * c->mlx.img.line_length + x
-				* (c->mlx.img.bits_per_pixel) / 8);
-		*(unsigned int *) dst = color;
-	}
-}
-
-void	improved_mlx_pixel_put(t_image *img, int x, int y, int color)
-{
-	char	*dest;
-
-	dest = img->addr;
-	dest += (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int *)dest = color;
+	result->r = (color >> 16) & 0xFF;
+	result->g = (color >> 8) & 0xFF;
+	result->b = color & 0xFF;
 }
 
 int	get_rgb(int r, int g, int b)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:07:58 by etomiyos          #+#    #+#             */
-/*   Updated: 2023/03/13 18:02:16 by mtomomit         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:13:26 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,28 +31,28 @@
 
 void	init_texture(t_cub3d *c)
 {
-	c->mlx.ea_tex.img.image = mlx_xpm_file_to_image(c->mlx.ptr, \
+	c->mlx.ea_tex.img.ptr = mlx_xpm_file_to_image(c->mlx.ptr, \
 		c->map.ea_path, &c->mlx.ea_tex.width, &c->mlx.ea_tex.height);
 	c->mlx.ea_tex.img.addr = \
-		mlx_get_data_addr(c->mlx.ea_tex.img.image, \
+		mlx_get_data_addr(c->mlx.ea_tex.img.ptr, \
 			&c->mlx.ea_tex.img.bits_per_pixel, \
 			&c->mlx.ea_tex.img.line_length, &c->mlx.ea_tex.img.endian);
-	c->mlx.no_tex.img.image = mlx_xpm_file_to_image(c->mlx.ptr, \
+	c->mlx.no_tex.img.ptr = mlx_xpm_file_to_image(c->mlx.ptr, \
 		c->map.no_path, &c->mlx.no_tex.width, &c->mlx.no_tex.height);
 	c->mlx.no_tex.img.addr = \
-		mlx_get_data_addr(c->mlx.no_tex.img.image, \
+		mlx_get_data_addr(c->mlx.no_tex.img.ptr, \
 			&c->mlx.no_tex.img.bits_per_pixel, \
 			&c->mlx.no_tex.img.line_length, &c->mlx.no_tex.img.endian);
-	c->mlx.so_tex.img.image = mlx_xpm_file_to_image(c->mlx.ptr, \
+	c->mlx.so_tex.img.ptr = mlx_xpm_file_to_image(c->mlx.ptr, \
 		c->map.so_path, &c->mlx.so_tex.width, &c->mlx.so_tex.height);
 	c->mlx.so_tex.img.addr = \
-		mlx_get_data_addr(c->mlx.so_tex.img.image, \
+		mlx_get_data_addr(c->mlx.so_tex.img.ptr, \
 			&c->mlx.so_tex.img.bits_per_pixel, \
 			&c->mlx.so_tex.img.line_length, &c->mlx.so_tex.img.endian);
-	c->mlx.we_tex.img.image = mlx_xpm_file_to_image(c->mlx.ptr, \
+	c->mlx.we_tex.img.ptr = mlx_xpm_file_to_image(c->mlx.ptr, \
 		c->map.we_path, &c->mlx.we_tex.width, &c->mlx.we_tex.height);
 	c->mlx.we_tex.img.addr = \
-		mlx_get_data_addr(c->mlx.we_tex.img.image, \
+		mlx_get_data_addr(c->mlx.we_tex.img.ptr, \
 			&c->mlx.we_tex.img.bits_per_pixel, \
 			&c->mlx.we_tex.img.line_length, &c->mlx.we_tex.img.endian);
 }
