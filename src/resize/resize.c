5e4f2db747752f29f9a82d69d2845f6c1fcab199
Matomomitsu <matomomitsu@gmail.com>
2023-03-17 13:11:16 -0300

    fixed norminette

--- src/resize/resize.c
+++ src/resize/resize.c
@@ -6,7 +6,7 @@
 /*   By: mtomomit <mtomomit@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:15:35 by mtomomit          #+#    #+#             */
-/*   Updated: 2023/03/17 12:25:30 by mtomomit         ###   ########.fr       */
+/*   Updated: 2023/03/17 13:10:53 by mtomomit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,12 @@ void	resize_window(t_cub3d *c)
 			&c->mlx.img.endian);
 }
 
-void	resize_menu(t_cub3d *c)
+void	resize_count(t_cub3d *c, int x, int y)
 {
-	int					x;
-	int					y;
 	int					sqrx;
 	int					sqry;
 	unsigned int		color;
 
-	x = 0;
-	c->menu.width_ratio = (float) c->mlx.img.win_width / c->menu.img.win_width;
-	c->menu.height_ratio = (float) c->mlx.img.win_height / c->menu.img.win_height;
-	get_btn_pos(&c->menu.quit, (int)(c->menu.quit.x * c->menu.width_ratio), (int)(c->menu.quit.y * c->menu.height_ratio));
-	get_btn_size(&c->menu.quit, (int)(c->menu.quit.width * c->menu.width_ratio), (int)(c->menu.quit.height * c->menu.height_ratio));
 	while (x < c->menu.img.win_width)
 	{
 		y = 0;
@@ -62,19 +55,32 @@ void	resize_menu(t_cub3d *c)
 	}
 }
 
-int	change_win_size(t_cub3d *c)
+void	resize_menu(t_cub3d *c)
 {
-	int	width;
-	int	height;
+	int	x;
+	int	y;
 
-	c->menu.fullscreen.toggle = !c->menu.fullscreen.toggle;
+	x = 0;
+	c->menu.width_ratio = (float) c->mlx.img.win_width / c->menu.img.win_width;
+	c->menu.height_ratio = (float) c->mlx.img.win_height \
+		/ c->menu.img.win_height;
+	get_btn_pos(&c->menu.quit, (int)(c->menu.quit.x * c->menu.width_ratio), \
+		(int)(c->menu.quit.y * c->menu.height_ratio));
+	get_btn_size(&c->menu.quit, (int)(c->menu.quit.width * \
+		c->menu.width_ratio), \
+		(int)(c->menu.quit.height * c->menu.height_ratio));
+	resize_count(c, x, y);
+}
+
+static void	get_width_and_height(t_cub3d *c, int *width, int *height)
+{
 	if (c->menu.fullscreen.toggle == TRUE)
 	{
-		width = c->mlx.screen_width;
-		height = c->mlx.screen_height;
+		*width = c->mlx.screen_width;
+		*height = c->mlx.screen_height;
 		if (!c->menu.resize.ptr)
 		{
-			c->menu.resize.ptr = mlx_new_image(c->mlx.ptr, width, height);
+			c->menu.resize.ptr = mlx_new_image(c->mlx.ptr, *width, *height);
 			c->menu.resize.addr = mlx_get_data_addr(c->menu.resize.ptr,
 					&c->menu.resize.bits_per_pixel, &c->menu.resize.line_length,
 					&c->menu.resize.endian);
@@ -82,9 +88,18 @@ int	change_win_size(t_cub3d *c)
 	}
 	else
 	{
-		width = WIDTH;
-		height = HEIGHT;
+		*width = WIDTH;
+		*height = HEIGHT;
 	}
+}
+
+int	change_win_size(t_cub3d *c)
+{
+	int	width;
+	int	height;
+
+	c->menu.fullscreen.toggle = !c->menu.fullscreen.toggle;
+	get_width_and_height(c, &width, &height);
 	mlx_destroy_image(c->mlx.ptr, c->mlx.img.ptr);
 	mlx_destroy_window(c->mlx.ptr, c->mlx.win.ptr);
 	c->mlx.win.width = width;
